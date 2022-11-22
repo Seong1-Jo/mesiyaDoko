37e3a1dbe2c4ff7e5d08e5b028d8d579613b9245
Seong1-Jo <seong12yada@gmail.com>
2022-11-22 23:14:54 +0900

    feat: Get My Location
    Get my latitude,longitude from Geolocation API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_one = ""; //API
 
 function App() {
+  const [latitude, setLatitude] = useState(""); //緯度(위도) state
+  const [longitude, setLongitude] = useState(""); //経度(경도)
+  const [name, setName] = useState([]);
+  const getNames = () => {
+    axios
+      .get(
+        `/hotpepper/gourmet/v1/?key=6512a79e28669890&lat=34.67&lng=135.52&range=5&order=4&format=json`
+      )
+      .then((response) => {
+        console.log("res", response.data.results.shop);
+        // setName(response);
+      });
+  };
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      console.log(position.coords);
+    });
+    getNames();
+  }, []);
+
+  console.log("name", name);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>현재 위도는: {latitude}</div>
+      <div>현재 경도는: {longitude}</div>
+      <div>현재위치로부터의 검색 반경을 지정</div>
+      <button>10</button>
+      <button>20</button>
+      <button>30</button>
+      <div>
+        {name
+          .filter((x) => x.name === "shop") //map의 조건문을 할때
+          .map((x) => (
+            <div>{x.name}</div> //key값은 일단 보류
+          ))}
+      </div>
+    </>
   );
 }
 
