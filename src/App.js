049fea383b32043aa26db13f2ed5362f06977ec6
Seong1-Jo <seong12yada@gmail.com>
2022-11-28 22:22:11 +0900

    feat: Add Shop List Form
    Add 一覧形式, styled-components Libray

--- src/App.js
+++ src/App.js
@@ -1,75 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./page/Home";
+import Detail from "./page/Detail";
 import Shop from "./component/Shop";
 
-const API_one = ""; //API
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [latitude, setLatitude] = useState(""); //緯度(위도) state
-  const [longitude, setLongitude] = useState(""); //経度(경도)
-  const [name, setName] = useState([]);
-  const [rangeClick, setRangeClick] = useState(3);
-
-  //asyan await으로 나중에할수있을때 하기
-  const getNames = () => {
-    axios
-      .get(
-        `/hotpepper/gourmet/v1/?key=6512a79e28669890&lat=34.67&lng=135.52&range=${rangeClick}&order=4&format=json`
-      )
-      .then((response) => {
-        console.log("샵이름", response.data.results);
-        setName(response.data.results.shop);
-      });
-  };
-  //현재위치를 가져오면서 api의 현재위치 함수 실행
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      //現在位置
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-      console.log(position.coords);
-    });
-    getNames();
-  }, [rangeClick]);
-
-  console.log("name", name);
   return (
-    <>
-      <div>현재 위도는: {latitude}</div>
-      <div>현재 경도는: {longitude}</div>
-      <div>현재위치로부터의 검색 반경을 지정</div>
-      <button
-        onClick={(e) => {
-          setRangeClick(1);
-          setLoading(false);
-        }}
-      >
-        300m
-      </button>
-      <button>500m</button>
-      <button>1000m</button>
-      <button>2000m</button>
-      <button>3000m</button>
-      <div>
-        {loading ? (
-          <h1>
-            현재위치는 위도 : {latitude} 경도 : {longitude} 입니다
-          </h1>
-        ) : (
-          name.map((shop) => (
-            <Shop
-              key={shop.id}
-              thumbnailImage={shop.logo_image}
-              shopName={shop.name}
-              shopAccess
-            />
-          ))
-        )}
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/:id" element={<Shop />} />
+        </Route>
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/component/Shop.js
+++ src/component/Shop.js
@@ -1,14 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 
-function Shop({ thumbnailImage, shopName, shopAccess }) {
+import { Link } from "react-router-dom";
+
+/* styled-components */
+const ShopDiv = styled.div`
+  border: 2px solid blue;
+  position: relative;
+  flex: 1 1 28%;
+  padding: 15px;
+  margin: 10px;
+  display: flex;
+`;
+const ShopImage = styled.img`
+  border: 5px solid blue;
+  width: 30%;
+  margin-right: 10px;
+`;
+const ShopInformaiton = styled.div`
+  border: 1px solid red;
+  width: 50%;
+`;
+
+const ShopName = styled.h4`
+  /* font-size: 20px; */
+`;
+const ShopAccess = styled.span`
+  font-size: 12px;
+  text-overflow: ellipsis;
+`;
+
+function Shop({ thumbnailImage, shopName, shopAccess, shopRange }) {
   return (
-    <>
-      <div>
-        <img src={thumbnailImage} alt={shopName} />
-        <h2>{shopName}</h2>
-        <p>{shopAccess}</p>
-      </div>
-    </>
+    <ShopDiv>
+      <ShopImage src={thumbnailImage} alt={shopName} />
+      <ShopInformaiton>
+        <ShopName>{shopName}</ShopName>
+        <ShopAccess>{shopAccess}</ShopAccess>
+      </ShopInformaiton>
+    </ShopDiv>
   );
 }
 
