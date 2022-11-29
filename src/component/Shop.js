28bcb3ae003dd785f7e104f6775e7d0b8de73db7
Seong1-Jo <seong12yada@gmail.com>
2022-11-29 23:07:19 +0900

    feat: Add Detail to Shop Items
    Add Detail.js, Get ShopId using useParams

--- src/component/Shop.js
+++ src/component/Shop.js
@@ -5,16 +5,17 @@ import { Link } from "react-router-dom";
 
 /* styled-components */
 const ShopDiv = styled.div`
-  border: 2px solid blue;
+  border: 2px solid orange;
   position: relative;
-  flex: 1 1 28%;
+  /* flex: 1 1 28%; */
   padding: 15px;
-  margin: 10px;
+  margin: 0 auto;
+  margin-top: 10px;
   display: flex;
+  width: 700px;
 `;
 const ShopImage = styled.img`
-  border: 5px solid blue;
-  width: 30%;
+  width: 10%;
   margin-right: 10px;
 `;
 const ShopInformaiton = styled.div`
@@ -30,12 +31,14 @@ const ShopAccess = styled.span`
   text-overflow: ellipsis;
 `;
 
-function Shop({ thumbnailImage, shopName, shopAccess, shopRange }) {
+function Shop({ thumbnailImage, shopName, shopAccess, shopRange, id }) {
   return (
     <ShopDiv>
       <ShopImage src={thumbnailImage} alt={shopName} />
       <ShopInformaiton>
-        <ShopName>{shopName}</ShopName>
+        <ShopName>
+          <Link to={`/detail/${id}`}>{shopName}</Link>
+        </ShopName>
         <ShopAccess>{shopAccess}</ShopAccess>
       </ShopInformaiton>
     </ShopDiv>
